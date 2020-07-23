e2afc3999876a04ef7f3bec0410095058968bcdb
Omarosh <omarkhattab59@yahoo.com>
2020-07-23 18:40:53 +0200

    Titles done for all pages

--- src/routes/[state].svelte
+++ src/routes/[state].svelte
@@ -12,7 +12,16 @@
   export let state;
 </script>
 
-<h1>{state}</h1>
+<svelte:head>
+  <title>Covid19 -{state}</title>
+</svelte:head>
+
+<div class="section header">
+  <div class="container">
+    <h1>Covid19 - {state}</h1>
+
+  </div>
+</div>
 
 <CovidStat />
 <CovidChart />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,5 +4,18 @@
   import TableContainer from "../components/TableContainer.svelte";
 </script>
 
+<svelte:head>
+  <title>Covid19 US Tracker</title>
+</svelte:head>
+
+<div class="section header">
+  <div class="container">
+    <h1>Covid19 - US</h1>
+
+  </div>
+</div>
 
 <TableContainer />
+
+<CovidChart />
+<CovidStat />
