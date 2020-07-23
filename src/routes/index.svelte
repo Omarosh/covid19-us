bb55560479a2d7efb5696543ab9c1a17529754b8
Omarosh <omarkhattab59@yahoo.com>
2020-07-23 15:12:47 +0200

    got pages and components working

---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<script>
+  import CovidStat from "../components/CovidStat.svelte";
+  import CovidChart from "../components/CovidChart.svelte";
+  import TableContainer from "../components/TableContainer.svelte";
+</script>
+
+
+<TableContainer />
